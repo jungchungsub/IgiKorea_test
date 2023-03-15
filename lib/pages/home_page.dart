a1d83d8b5f6fd893f0ca787d6255c105747c4316
jungchungsub <cndtjq1248@naver.com>
2023-03-15 12:37:02 +0900

    기본ui/ux 작성

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,9 +15,64 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: gBackgroundColor,
       body: ListView(
         children: [
-          Container(
-            child: Row(
-              children: [],
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 9, bottom: 9, left: 18, right: 18),
+            child: Container(
+              decoration: BoxDecoration(
+                color: gItemViewColor,
+                borderRadius: BorderRadius.circular(14),
+              ),
+              height: 126,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8, bottom: 8, left: 5),
+                    child: Image.asset(
+                      "assets/main_vp_1.png",
+                      fit: BoxFit.cover,
+                      height: 110,
+                      width: 126,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16, left: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: gEventProgressColor,
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "예정",
+                              style: TextStyle(
+                                  color: gEventTextColor, fontSize: 9),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 7),
+                        Text(
+                          "구간 4차 - 철원",
+                          style: TextStyle(
+                              color: gMainTextColor,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 7),
+                        Text(
+                          "행사기간: 11/4 ~ 11/6",
+                          style: TextStyle(color: gSubTextColor, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
